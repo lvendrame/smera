7b94960a265be83c0c33c328087c4924f000b2c5
lvendrame <lvendrame@gmail.com>
2015-11-27 16:26:09 -0200

    Remove q, use mongoose native promise

--- test/repositoryTest.js
+++ test/repositoryTest.js
@@ -8,6 +8,9 @@ var modelRep = new Repository(model);
 
 var firstItem = null;
 var secondItem = null;
+var onError = function(err) {
+  console.log(err);
+};
 
 modelRep.create({
 	name : 'Teste Model',
@@ -21,27 +24,25 @@ modelRep.create({
   	age : 45,
     createdAt: new Date()
   });
-})
+}, onError)
 .then(function(item){
   secondItem = item;
   return modelRep.get();
-})
+}, onError)
 .then(function(items){
   console.log(items.length);
   return modelRep.getById(firstItem._id);
-})
+}, onError)
 .then(function(item){
   console.log(item);
   item.age = 22;
   return modelRep.update(item);
-})
+}, onError)
 .then(function(item){
   console.log(item);
   return modelRep.delete(secondItem);
 })
 .then(function(item){
   console.log(item);
-})
-.catch(function(err) {
-  console.log(err);
-});
+}, onError)
+;
